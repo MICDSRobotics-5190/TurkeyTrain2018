a37567b93b4cb58fdb126b8b9c2f9fa3a8085834
Nick Clifford <nick@nickclifford.me>
2018-11-05 13:45:57 -0600

    basic opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurkeyTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurkeyTrain.java
@@ -1,6 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
+@TeleOp(name = "Turkey Train", group = "Driver")
 public class TurkeyTrain extends OpMode {
+    private DcMotor leftMotor;
+    private DcMotor rightMotor;
+
+    @Override
+    public void init() {
+        leftMotor = hardwareMap.dcMotor.get("left");
+        rightMotor = hardwareMap.dcMotor.get("right");
+        leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    @Override
+    public void loop() {
+        leftMotor.setPower(gamepad1.left_stick_y);
+        rightMotor.setPower(gamepad1.right_stick_y);
+    }
 }
